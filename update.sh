c437e0b60401e1e50d9829c7aac2af6d076cf50a
tinywrkb <tinywrkb@gmail.com>
2022-05-09 03:02:02 +0300

    Packaging improvements, and bump to latest

--- update.sh
+++ update.sh
@@ -4,6 +4,20 @@ _FPID=org.flatpak.flatpak-builder-tools
 
 [ -f ${_FPID}.yaml ] || { echo "Can't find ${_FPID}.yaml"; exit 1; }
 
-flatpak run --filesystem=$PWD org.flathub.flatpak-external-data-checker --edit-only ${_FPID}.yaml
-./tools/pip-updater
-./tools/cpan-updater
+_TOOLSDIR=$PWD/tools
+
+flatpak run \
+  --runtime=org.freedesktop.Sdk//21.08 \
+  --filesystem=$PWD \
+  org.flathub.flatpak-external-data-checker \
+  --edit-only ${_FPID}.yaml
+
+# python modules with multiple dependencies and have a requirements.txt file
+for _mod in python-aiohttp; do
+  (
+    cd $_mod
+    ${_TOOLSDIR}/pip-updater $(basename $_mod)
+  )
+done
+
+${_TOOLSDIR}/cpan-updater
